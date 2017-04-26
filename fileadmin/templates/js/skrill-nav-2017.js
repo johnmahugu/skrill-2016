06af0c0e39db56c725cb5004e3ec11a830eaf58e
radogado <rado@me.com>
2017-04-26 12:08:15 +0300

    ...

---
+++ fileadmin/templates/js/skrill-nav-2017.js
@@ -0,0 +1,23 @@
+$('header nav li.sub, header nav li.sub *').on('touchend', function (e) { // Toggle sub nav on touch
+	
+	e.stopPropagation();
+	if ($(this).hasClass('sub') || $(this).parent().hasClass('sub language')) {
+	
+		$(this).closest('.sub').toggleClass('open');
+		return false;
+	
+	}
+
+});
+
+function closeFoldClickOutside(e) { // Close menu when clicking/tapping outside
+	
+	if (!e.target.closest('header')) {
+		
+		$('#toggle-menu')[0].checked = false;
+		
+	}
+	
+}
+
+$(window).on('click touchend', closeFoldClickOutside);
