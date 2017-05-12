8301ff99b147cb24f735162c8189650a8439c14e
radogado <rado@me.com>
2017-05-12 14:07:27 +0300

    ...

--- fileadmin/templates/js/skrill-nav-2017.js
+++ fileadmin/templates/js/skrill-nav-2017.js
@@ -1,4 +1,4 @@
-// Real time touch detection to support devices with both touch and mouse. http://www.javascriptkit.com/dhtmltutors/sticky-hover-issue-solutions.shtml
+// Realtime touch detection to support devices with both touch and mouse. http://www.javascriptkit.com/dhtmltutors/sticky-hover-issue-solutions.shtml
 
 ;(function(){
 
@@ -54,8 +54,7 @@
 
 })();
 
-
-// Toggle sub nav on touch and remove the fallback checkboxes
+// Toggle sub nav on touch; removes the fallback checkboxes
 $('header input[type=radio]').remove();
 $('header nav li.sub, header nav li.sub *').on('touchend click', function (e) {
 	
@@ -69,8 +68,8 @@ $('header nav li.sub, header nav li.sub *').on('touchend click', function (e) {
 
 });
 
-
-function closeFoldClickOutside(e) { // Close menu when clicking/tapping outside
+// Close menu when clicking/tapping outside
+function closeFoldClickOutside(e) { 
 	
 	if (!e.target.closest('header')) {
 		
@@ -83,20 +82,19 @@ function closeFoldClickOutside(e) { // Close menu when clicking/tapping outside
 
 $(window).on('click touchend', closeFoldClickOutside);
 
-/*
 $(window).on('scroll', function (e) {
-	
-	if ($('#toggle-menu')[0].checked) {
 
-		$('#toggle-menu')[0].checked = false;
-		$('header').removeClass('open');
-		$('header .language').removeClass('open');
-		$('header').scrollTop(0);
-	
+	if (window.scrollY > window.innerHeight) {
+		
+		$('body').addClass('scrolled');
+
+	} else {
+		
+		$('body').removeClass('scrolled');
+
 	}
 	
 });
-*/
 
 $('#toggle-menu').on('change', function (e) {
 	
@@ -107,15 +105,71 @@ $('#toggle-menu').on('change', function (e) {
 
 function setHeaderVariables() {
 
-	$('header')[0].style.setProperty('--viewport-height', window.innerHeight + 'px');
+	$('header')[0].style.setProperty('--viewport-height', window.innerHeight + 'px'); // for proper Safari iOS toolbar on/off layout, because it misreports vh units
 	$('header nav li.sub:not(.language) ul').each( function (n) {
 		
 		$(this)[0].style.setProperty('--max-height', $(this)[0].scrollHeight + 'px'); // for height animation
 		
 	});
-	$('main').css('padding-top', $('header').height() + 'px');
+	$('main').css('padding-top', $('header').height() + 'px'); // Position content below the fixed header
 
 }
 
 $(window).on('resize', setHeaderVariables);
 setHeaderVariables();
+
+// @media media_type { ... } -> @media new_media_type { ... }
+function setMediaQueryType (media_type, new_media_type) { 
+	// bug: messes up the Safari inspector, but not others
+	// Loop all stylesheets' CSS rules with type 4 (media query) matching the media_type parameter and update their cssText with new_media_type
+
+	var sheet = 0;
+	while (sheet < document.styleSheets.length) {
+	
+		var styles = document.styleSheets[sheet];
+	
+		var i = 0;
+		while (i < styles.cssRules.length) {
+			
+			if (styles.cssRules[i].type === 4) {
+				
+				var new_media = styles.cssRules[i].cssText.replace(media_type, new_media_type); // to do: limit search to the type section, don't process the rules inside { } 
+				styles.deleteRule(i);
+				styles.insertRule(new_media, i);
+	
+			}
+			
+			i++;
+			
+		}
+		
+		sheet++;
+	
+	}
+		
+}
+
+// Calculate required window width for unwrapped nav and set the breakpoint accordingly
+function setBreakpoint() {
+	
+	var breakpoint = 901;
+	$('header .middle').addClass('absolute');
+	var middle_width = $('header .middle')[0].scrollWidth;
+	$('header .middle').removeClass('absolute');
+	var new_breakpoint = $('header .left')[0].scrollWidth + $('header .right')[0].scrollWidth + middle_width + 16;
+
+	if (new_breakpoint > breakpoint) {
+
+		setMediaQueryType ('width: ' + breakpoint + 'px)', 'width: ' +  new_breakpoint + 'px)');
+
+	}
+
+}
+
+$(window).load(function() { // Executes when complete page is fully loaded, including all frames, objects and images. 
+
+	setBreakpoint();
+
+});
+
+$(window).on('resize', setBreakpoint);
